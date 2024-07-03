79b0ad1860089672ee62a6c6330d87b4331e6346
yuzhaojun <yuzhaojun@kuaishou.com>
2024-07-03 23:35:12 +0800

    feat: 初始化组件开发

--- README.md
+++ README.md
@@ -12,8 +12,25 @@ yarn start
 
 # 开发规范
 
+### 分支开发规范
+
 主分支 main
 
 测试分支 feature
 
 研发个人的分支从 feature 拉取分支，开发完后合入到 feature 分支
+
+### 代码开发规范
+
+开发组件
+
+1. 通用组件
+
+直接在 src/components 目录下创建组件
+
+2. 业务组件
+
+在对应页面文件夹下的 components 目录下创建组件
+
+例如：src/home/components/xxx.tsx
+
---
+++ app/home/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home</Text>
+    </View>
+  );
+}
