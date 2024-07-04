dbc36f56fd7bbbc841fe6c0aaea9a73c8af42518
yuzhaojun <yuzhaojun@kuaishou.com>
2024-07-04 15:28:42 +0800

    feat: 放弃无用代码

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,9 @@
 import { Stack } from "expo-router";
-import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        headerStyle: {
-          backgroundColor: '#000',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
+    <Stack>
+      <Stack.Screen name="index" />
     </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Index Page</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
