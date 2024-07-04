88370e0d8d5320c203fbf93dc9fc51ca11677ff3
yuzhaojun <yuzhaojun@kuaishou.com>
2024-07-04 15:02:57 +0800

    feat: 更新路由

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,39 @@
 import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        headerStyle: {
+          backgroundColor: '#000',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
