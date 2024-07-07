3e6fd57114a737d1e7fedf9939d67cdaaa665dcd
yuzhaojun <yuzhaojun@kuaishou.com>
2024-07-07 16:40:31 +0800

    feat : 增加地图组件

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+
+async function getCurrentLocation() {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    console.log('位置权限被拒绝');
+    return;
+  }
+
+  let location = await Location.getCurrentPositionAsync({});
+  console.log(location);
+  return location;
+}
+
+// 定义 Region 接口来描述 region 状态的结构
+interface Region {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
 
 export default function CreateScreen() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+
+  useEffect(() => {
+    getCurrentLocation().then((local) => {
+      console.log(local, 'setLocationsetLocationsetLocation');
+      local && setLocation(local);
+    });
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 39.9042,
+        longitude: 116.4074,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
       }}
+      showsUserLocation={true}
+      followsUserLocation={true} // 地图跟随用户位置变化
+      mapType="standard" // 标准地图类型
     >
-      <Text>Create</Text>
-    </View>
+      {location && (
+        <Marker
+          coordinate={{
+            latitude: 39.909,
+            longitude: 116.3975,
+          }}
+          title="当前位置"
+        />
+      )}
+    </MapView>
   );
 }
