04a7d5e969daf23626c10202f646810b8e23cd5c
ben100118 <ben100118@163.com>
2024-08-13 09:51:27 -0700

    feat: 更改卡片样式

--- components/Primary.tsx
+++ components/Primary.tsx
@@ -12,15 +12,21 @@ export default function Primary() {
       <View style={{ marginBottom: 15 }}>
         <FirstCard
           imageSource={require('@/assets/images/createTravel.png')}
-          buttonText="+ 创建行程"
+          mainText="快捷创建"
           routerSource="create"
+          subText="简易流程、好友共建"
+          buttonText="智能创建"
+          imageStyle={{ width: 250, height: 250, resizeMode: 'contain' , marginTop: 120, marginLeft: 20}}
         />
       </View>
       <View style={{ marginTop: 15 }}>
         <FirstCard
           imageSource={require('@/assets/images/createAI.png')}
-          buttonText="+ AI解析"
+          mainText="智能创建"
           routerSource="./ailink"
+          subText="AI链接解析、AI一键生成"
+          buttonText="AI解析"
+          imageStyle={{ width: 250, height: 250, resizeMode: 'contain', marginTop: 120, marginRight: 20}}
         />
       </View>
     </View>
