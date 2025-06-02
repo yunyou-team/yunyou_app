a264609c30df1d676b0a94fb407e861f9c3e2c17
zhaoqi09 <zhaoqi09@kuaishou.com>
2025-06-02 23:33:55 +0800

    feat: 升级expo53

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,60 +1,60 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+// import { NavigationContainer } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { View } from 'react-native'; 
+import { View } from 'react-native';
 import { storage } from '@/utils';
-import FirstLogin from './components/login/FirstLoading'
+import FirstLogin from './components/login/FirstLoading';
 
 export default function Layout() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [initialRoute, setInitialRoute] = useState<string | null>('(app)');
+    const [isLoading, setIsLoading] = useState(true);
+    const [initialRoute, setInitialRoute] = useState<string | null>('(app)');
 
-  useEffect(() => {
-    checkInitialRoute();
-  }, []);
+    useEffect(() => {
+        checkInitialRoute();
+    }, []);
 
-  const checkInitialRoute = async () => {
-    try {
-      const cookie = await storage.get('cookie');
-      // 模拟验证冷启登录态
-      await new Promise(resolve => setTimeout(resolve, 3000));
-      setInitialRoute(cookie ? '(tabs)' : '(app)');
-    } catch (error) {
-      console.error('Error checking cookie:', error);
-      setInitialRoute('(app)');
-    } finally {
-      setIsLoading(false);
-      console.log('加载完成');
-    }
-  };
+    const checkInitialRoute = async () => {
+        try {
+            const cookie = await storage.get('cookie');
+            // 模拟验证冷启登录态
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+            setInitialRoute(cookie ? '(tabs)' : '(app)');
+        } catch (error) {
+            console.error('Error checking cookie:', error);
+            setInitialRoute('(app)');
+        } finally {
+            setIsLoading(false);
+            console.log('加载完成');
+        }
+    };
 
-  return (
-    <View style={{ flex: 1 }}>
-          <NavigationContainer>
-            <Stack 
-              screenOptions={{ 
-                headerShown: false,
-                gestureEnabled: false,
-              }}
+    return (
+        <View style={{ flex: 1 }}>
+            {/* react-navigation 升级 去除 NavigationContainer */}
+            {/* <NavigationContainer> */}
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    gestureEnabled: false,
+                }}
             >
-              <Stack.Screen 
-                name="(tabs)" 
-                options={{ 
-                  headerShown: false,
-                  gestureEnabled: false  
-                }} 
-              />
-              <Stack.Screen 
-                name='(app)' 
-                options={{ 
-                  headerShown: false,
-                  gestureEnabled: true,
-                }} 
-              />
+                <Stack.Screen
+                    name='(tabs)'
+                    options={{
+                        headerShown: false,
+                        gestureEnabled: false,
+                    }}
+                />
+                <Stack.Screen
+                    name='(app)'
+                    options={{
+                        headerShown: false,
+                        gestureEnabled: true,
+                    }}
+                />
             </Stack>
-          </NavigationContainer>
-       
-    </View>
-  );
+            {/* </NavigationContainer> */}
+        </View>
+    );
 }
